73701197e6fa3e91fba03cf26a0b23c8ee927885
Piotr <flisu2@gmail.com>
2025-03-04 16:50:52 +0100

    fixed hammer

--- candles/hammer.py
+++ candles/hammer.py
@@ -4,15 +4,13 @@ import pandas as pd
 
 def filter_hammers(dataset: pd.DataFrame)-> pd.DataFrame:
     columns = dataset.columns
-    print(columns)
     row_list = []
     for row in dataset.itertuples(index=False):
         timestamp, open_, high, low, close, volume = row
         candle_width = abs(open_ - close)
-        high_width = high - open_ if high - open_ < high - close else high - close
-        shadow = high - candle_width - candle_width
-
-        if shadow >= candle_width  and high_width < 0.5 * candle_width:
+        lower_shadow = open_ - low if open_ > close else close - low
+        upper_shadow = high - max(open_, close)
+        if lower_shadow >= candle_width * 2 and upper_shadow <= candle_width * 0.2:
             temp_dict = {
                 columns[0] : timestamp,
                 columns[1]: open_,
@@ -25,9 +23,6 @@ def filter_hammers(dataset: pd.DataFrame)-> pd.DataFrame:
     return pd.DataFrame(row_list)
 
 
-data = pd.DataFrame(columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-data.loc[-1] = [0, 15, 20, 10, 19,1 ]
-print(len(filter_hammers(data)))
 
 
 print(len(filter_hammers(pd.read_csv('../data/btc_1h.csv'))))
